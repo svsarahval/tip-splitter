dc6b3ed24ab9b625e3dc5173af32bfc3b0f4ead0
svsarahval <svsarahval@gmail.com>
2023-01-09 20:23:25 -0500

    just setting things up

---
+++ script.js
@@ -0,0 +1,24 @@
+// TODO: Handle user input of initial bill value
+// TODO: Handle user selection of the gratuity, both default and custom values
+// TODO: Handle user input of number of people to split the bill between
+// TODO: Handle calculation of tip per person and total bill per person
+// TODO: Handle resetting the calculator
+// TODO: Handle appropriate styling for gratuities
+// -> If a pre-set gratuity is selected, it should appear active.
+// -> If no a pre-set gratuity is selected, or the user is providing a custom tip,
+//    the buttons should NOT have appear active
+// TODO: Handle appropriate styling for reset btn
+// -> If there has been no value calculated, the reset btn should not work
+// -> If the tips have been calculated, the reset btn should work
+
+// Note: The elements needed have been queried for you here
+// ** Query elements
+const bill = document.getElementById('bill');
+const gratuityBtns = document.querySelectorAll('.gratuity');
+const customGratuity = document.getElementById('custom-gratuity');
+const people = document.getElementById('people');
+const splitTip = document.getElementById('split-tip');
+const splitTotal = document.getElementById('split-total');
+const resetBtn = document.getElementById('reset');
+
+// ** Your work goes below here
