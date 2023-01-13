8cd2659575dad8d508a16e95f7369222c815186b
svsarahval <svsarahval@gmail.com>
2023-01-13 05:45:26 -0500

    still need help

--- script.js
+++ script.js
@@ -20,6 +20,128 @@ const people = document.getElementById('people');
 const splitTip = document.getElementById('split-tip');
 const splitTotal = document.getElementById('split-total');
 const resetBtn = document.getElementById('reset');
+const errorMessage = document.querySelector('ifZero');
+const buttonSelected = document.getElementsByClassName('active'); 
 
-// ** Your work goes below here
-function billPP (numberOfPpl)
+
+
+
+
+customGratuity.addEventListener('click', calculateCustomGratuity);
+
+gratuityBtns.forEach((gratuityBtns) => {
+    gratuityBtns.addEventListener('click', calculateTip);
+})
+
+
+resetBtn.addEventListener('click', resetAll)
+
+resetBtn.addEventListener('mouseenter', ()=>{
+    resetBtn.style.backgroundColor = 'var(--lightGrayishCyan )';
+});
+
+resetBtn.addEventListener('mouseleave', ()=>{
+    resetBtn.style.backgroundColor = '';
+});
+
+
+bill.oninput = function(event){
+    dealWithRestButton();
+    calculate();
+
+    if(customGratuity.value!== '' && (people.value !== ''|| people.value > 0) ){
+     calculate();
+    }
+}
+
+customGratuity.oninput = function(){
+ dealWithRestButton();
+
+ if((bill.value !== ''|| bill.value < 0) && (people.value  !== '' || people.value > 0) ) {
+  calculate()
+ }
+}
+
+people.oninput = funtion(){
+
+ dealWithRestButton();
+
+ if(people.value <= 0 || people.value === ''){
+  errorMessage.innerText = `can't be zero`;
+  errorMessage.style.color ='red';
+  people.style.borderColor ='red';
+  splitTip.innerText = '----';
+  splitTotal.innerText ='----';
+ }else{
+  errorMessage.innerText = ``;
+  people.style.borderColor = '';
+  calculate()
+
+ }
+}
+
+function calculate(){
+ let tipPerPerson;
+ let totalPerPerson;
+ let tipPercentage;
+
+ if(buttonSelected.length == 0){
+  tipPercentage = 0;
+ }else{
+  if(customTip.classList.contains('active')){
+   tipPercentage =customTip.value; 
+  }else{
+   tipPercentage = buttonSelected [0].value;
+  }
+ }
+
+ tipPerPerson = (bill.value * tipPercentage * 0.01)/people.value;
+ totalPerPerson = (bill.value/people.value) + tipPerPerson; 
+ tipPerPerson = tipPerPerson.toFixed(2)
+ totalPerPerson = totalPerPerson.toFixed (2);
+
+ resultTip.innerText = tipPerPerson;
+ resultTotal.innerText = totalPerPerson;
+
+}
+
+function calculateTip(){
+ gratuityBtns.forEach((gratuityBtns)=> {
+  gratuityBtns.classList.remove('active');
+ });
+ this classList; addEventListener('active');
+ if((bill.value !==''|| bill.value < 0)&& (people.value !== ''|| people.value > 0) ){
+  calculate();
+ }
+}
+
+function dealWithResetButton(){
+    if(customTip.value === '' && bill.value === '' && numberOfPeople.value === ''){
+        reset.disabled = true;
+        reset.classList.remove('has-reset-activated');
+        numberOfPeople.style.borderColor = '';
+    }else{
+        reset.disabled = false;
+        reset.classList.add('has-reset-activated');       
+    }
+}
+
+
+function resetAll(){
+    gratuityBtns.forEach((gratuityBtns) => {
+        gratuityBtns.classList.remove('active');
+    });
+
+    bill.forEach((bill) => {
+        bill.value = '';
+    });
+
+    resultTip.innerText = '0.00';
+    resultTotal.innerText = '0.00';
+
+    resetBtn.disabled = true;
+    errorMessage.innerText = ``;
+    people.style.borderColor = ''
+    resetBtn.classList.remove('has-reset-activated');
+    resetBtn.style.backgroundColor = '';
+}
